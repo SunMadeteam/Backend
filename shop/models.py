1183eba1661df5889e64b13befa81ec2af37e5d0
Karina <rejobo6557@gmail.com>
2022-03-11 19:21:26 +0600

    Fixed bugs

--- shop/models.py
+++ shop/models.py
@@ -24,8 +24,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     CHOICES = ((1,'easy'),(2,'middle'), (3,'hard'))
     complexity_of_care= models.IntegerField(choices=CHOICES, default=None, null=True)
-    florist = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    hight= models.DecimalField(max_digits=10, decimal_places=2)
+    florist = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
+    hight= models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     
     #def get_absolute_url(self):
     #    return reverse('shop:product_list_by_category', args=[self.slug])
